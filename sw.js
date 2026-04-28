0111449656953765f85a28901489472437f4ceaa
RadishZzzZ <153790260+RadishZzzZ@users.noreply.github.com>
2026-04-28 15:55:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,51 @@
+/*
+sw.js
+============================================================
+这是 Service Worker 文件。
+它让浏览器知道这个网页可以作为 PWA 使用，并缓存基础文件。
+============================================================
+*/
 
+const CACHE_NAME = "jp-workplace-prompt-pwa-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
